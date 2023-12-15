ccc43868edfd0603b16e9a4728866119ea432c70
maryammayowa001 <maryamabdulmajeed269@gmail.com>
2023-12-15 12:11:22 +0100

    number

---
+++ number checker.py
@@ -0,0 +1,9 @@
+#python program to check if a nunber is positive,negativeor zero
+while True:
+    num=int(input("Enter a number"))
+    if num>0:
+       print("The number is positive")
+    elif num==0:
+        print("The number is zero")
+    else:
+        print("The number is nagetive")
